4df972b42f554fa726b52dc65321fabab305f4b2
ucfAndresRosales <andres.rosales@knights.ucf.edu>
2021-07-01 18:21:05 -0400

    did plantuml

---
+++ src/main/java/ucf/assignments/csvHandler.java
@@ -0,0 +1,24 @@
+package ucf.assignments;
+
+import java.util.*;
+
+public class csvHandler {
+
+    public List readCSV(String filename){
+        List<TODO> todos = new ArrayList<TODO>();
+        //try and open file
+        //if not existent return empty List
+        //use csv parser to create todos List
+        return todos;
+    }
+
+    public void writeCSV(List<TODO> todos){
+        //check if file exists
+        //if does, ask user if they would like to overwrite
+        //if file doesnt exist or if yes, create file
+        //loop through list writing to file with commas,new line for every TODO
+    }
+    public void createFile(String filename){
+        //
+    }
+}
